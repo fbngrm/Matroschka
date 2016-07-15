8702ef6790e02b35d7c42f992e442f51511d05b3
Fabian Grimme <fbngrm@gmail.com>
2016-07-15 13:58:49 +0200

    Refactor structure & add image hiding

--- steganopy/aesteganohide.py
+++ steganopy/aesteganohide.py
@@ -17,12 +17,6 @@ def create_hmac(mac_pass, msg_bytes):
         mac_pass, msg_bytes, digestmod=hashlib.sha256).digest()
 
 
-def save_img(img):
-    name, ext = os.path.splitext(img)
-    secret.save('%s_%s' % (name, ext))
-    os.rename('%s_%s' % (name, ext), '%s.ste' % img)
-
-
 def get_msg(img):
     """
     Extract the hidden message fro the given image.
@@ -39,13 +33,13 @@ def get_msg(img):
     i.show()
 
 
-def check_hmac(hmac):
+def check_hmac(mac):
     """
     Check if the given hmac ist valid by creating
     a new hmac with the supplied password and the data.
     """
     h_mac = hmac.new(args['m'], bytes(data), digestmod=hashlib.sha256).digest()
-    print 'HMAC validation: %s\n' % str(h_mac == hmac)
+    print 'HMAC validation: \n%s\n' % str(h_mac == mac)
 
 
 def hash_128_bit_pass(passwd):
@@ -58,7 +52,7 @@ def hash_128_bit_pass(passwd):
     return h.hexdigest()[:16]
 
 
-def crypt(data, key, iv):
+def crypt(key, data, iv):
     """
     Encrypt or decrypt the given data with the given
     key using the XTEA algorithm in CFB mode.
@@ -87,12 +81,74 @@ def read_text(text_path):
     return open(text_path).read()
 
 
+def encrypt(data_type):
+    # create hmac from the supplied mac password hashed with SHA-256
+    # digest algorithm and the supllied image or text data
+    h_mac = create_hmac(args['m'], bytes(data))
+
+    # join hmac and message data
+    secret = '%s--:--%s' % (h_mac, data)
+
+    # create a SHA-256 hash of the high-order 128 bit of the given password
+    key = hash_128_bit_pass(args['k'])
+
+    # encrypt the data using XTEA algorithm in CFB mode with the 128 bit
+    # SHA-256 hash of the password as key and a random seed of 8 bytes
+    iv = os.urandom(8)
+    encrypted_secret = crypt(key, secret, iv)
+
+    # prepend the iv to the encrypted data because it is required for
+    # the decryption and hide the data in the image
+    matroschka = stg.hide_msg(image, '%s--:--%s--:--%s' % (
+        data_type, iv, encrypted_secret))
+
+    # save the image containing the embeded and encrypted
+    # data to disk with the extension .ste
+    name, ext = os.path.splitext(args['image'])
+    matroschka.save('%s_%s' % (name, ext))
+    os.rename('%s_%s' % (name, ext), '%s.ste' % args['image'])
+
+    print "successfully encrypted your secret message"
+    matroschka.show()
+
+
+def decrypt():
+    # extract and authenticate the hidden message in the image data
+    image = Image.open('%s.ste' % args['image'])
+    matroschka = stg.extract_msg(image)
+
+    # get the 8 byte iv and the encrypted secret from the image data
+    data_type, iv, encrypted_secret = matroschka.split('--:--')
+
+    # create a SHA-256 hash of the high-order 128 bit of the given password
+    key = hash_128_bit_pass(args['k'])
+
+    # decrypt the secret with the iv and the supplied password
+    decrypted_secret = crypt(key, encrypted_secret, iv)
+
+    # split hmac and message data
+    mac, data = decrypted_secret.split('--:--')
+
+    print 'HMAC hex is: \n%s\n' % mac.encode('hex')
+    check_hmac(mac)
+
+    if data_type == 'image':
+        ipath = "resources/secret-image.png"
+        print "the secret image is stored under: " + ipath
+        fh = open(ipath, "wb")
+        fh.write(data.decode('base64'))
+        fh.close()
+        Image.open(ipath).show()
+    else:
+        print 'The hidden message is: \n%s\n' % data
+
+
 if __name__ == '__main__':
     # Add the command-line arguments
     parser = argparse.ArgumentParser(description='Description of your program')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-e', help='encrypt', action='store_true')
-    group.add_argument('-d', help='decrypt', action='store_true')
+    group.add_argument('-hide', help='encrypt', action='store_true')
+    group.add_argument('-open', help='decrypt', action='store_true')
     parser.add_argument(
         '-m', metavar='macpasswd', help='macpassword', required=True)
     parser.add_argument('-k', metavar='passwd', help='password', required=True)
@@ -104,7 +160,17 @@ if __name__ == '__main__':
     # Check if the required args are supplied
     # If not print user feedback and exit
     if args['data']:
-        data = read_text(args['data'])
+        if args['data'].endswith('png') or args['data'].endswith('jpg'):
+            import base64
+            data_type = 'image'
+            with open(args['data'], "rb") as imageFile:
+                data = base64.b64encode(imageFile.read())
+        elif args['data'].endswith('txt'):
+            data_type = 'text'
+            data = read_text(args['data'])
+        else:
+            print "need secret message either as .txt or .png file"
+            sys.exit(0)
     else:
         print "need data to embed - either text or image"
         sys.exit(0)
@@ -115,27 +181,10 @@ if __name__ == '__main__':
         print "need image to embed data"
         sys.exit(0)
 
-    # create hmac from the supplied mac password hashed with SHA-256 
-    # digest algorithm and the supllied image or text data 
-    h_mac = create_hmac(args['m'], bytes(data))
+    # encrypt the secret message
+    if args['hide']:
+        encrypt(data_type)
 
-    #  
-    secret = stg.hide_msg(image, '%s--:--%s' % (h_mac, data))
-
-    # save the image containing the embeded and encrypted 
-    # data to disk with the extension .ste
-    save_img(args['image'])
-
-    # extract and authenticate the hidden message in the image data
-    get_msg(args['image'])
-
-    # create a SHA-256 hash of the high-order 128 bit of the given password
-    key = hash_128_bit_pass(args['k'])
-
-    # encrupy the data using the XTEA algorithm in CFB mode with the 128 bit
-    # SHA-256 hash of the password as key
-    encrypted_data = crypt(data, key)
-    iv = os.urandom(8)
-    print xtea.crypt(key, encrypted_data, iv)
-    print xtea.crypt(key, encrypted_data, iv) == data
-
+    # decrypt the secret message
+    if args['open']:
+        decrypt()
